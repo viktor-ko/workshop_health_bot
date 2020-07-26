3d09af6f88a579b687de6077fd105228d7c3b107
Vadim Egorov <stainwoortsel@gmail.com>
2020-07-26 12:36:12 +0300

    fix: no life token

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import telebot
 from dialog import Dialog
 
-token = '723314573:AAH5-onyzwyA1fMDnk73GLBQ7sE2a2j_qD4'
+token = '111111111:AAH5xxxxxxxxxxxxxxxxxxxxxxxxxxxxqD4'
 
 bot = telebot.TeleBot(token=token)
 
+print(f'Your token is: {token}')
 print('Start pooling...')
 
 dialog = Dialog(bot, {
